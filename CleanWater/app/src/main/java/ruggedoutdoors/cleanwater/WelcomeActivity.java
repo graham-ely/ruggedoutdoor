50820539cbbc576679e4ba919f6c94e4e282d622
austindunn7 <adunn@gatech.edu>
2017-02-13 19:40:53 -0500

    Added Welcome Page with login and register options

---
+++ CleanWater/app/src/main/java/ruggedoutdoors/cleanwater/WelcomeActivity.java
@@ -0,0 +1,39 @@
+package ruggedoutdoors.cleanwater;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Austin Dunn on 2/13/2017.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button mLoginButton = (Button) findViewById(R.id.login_button);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextScreen = new Intent(getApplicationContext(), LoginActivity.class);
+
+                startActivity(nextScreen);
+            }
+        });
+
+        Button mRegisterButton = (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+}
+
+}
