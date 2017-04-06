126ff32d1e20d99b15c3aba24834f14436b44281
austindunn7 <dkrux777@gmail.com>
2017-04-06 11:50:21 -0400

    javadocs update

--- CleanWater/app/src/main/java/ruggedoutdoors/cleanwater/model/Model.java
+++ CleanWater/app/src/main/java/ruggedoutdoors/cleanwater/model/Model.java
@@ -21,6 +21,7 @@ import android.util.Log;
 
 public class Model {
 
+    // database variables
     private static final String TAG = "Model";
     private DatabaseHelper mDbHelper;
     private static SQLiteDatabase mDb;
@@ -48,22 +49,35 @@ public class Model {
     private static double activeVPPM;
     private static double activeCPPM;
 
+    // database info variables
     private static String DB_PATH = "/data/data/ruggedoutdoors.cleanwater/databases/";
-
     private static final String DATABASE_NAME = "model";
-
     private static final int DATABASE_VERSION = 3;
 
+    // model context variable
     private final Context adapterContext;
 
+    /**
+     * default constructor
+     */
     public Model() {
         this.adapterContext = null;
     }
 
+    /**
+     * constructor
+     *
+     * @param context Context of where of the model is used
+     */
     public Model(Context context) {
         this.adapterContext = context;
     }
 
+    /**
+     * opens access to database
+     * @throws SQLException
+     * @return Model
+     */
     public Model open() throws SQLException {
         mDbHelper = new DatabaseHelper(adapterContext);
 
@@ -81,6 +95,9 @@ public class Model {
         return this;
     }
 
+    /**
+     * creates tables in database if not already created
+     */
     public void init() {
         String command1 = "create table if not exists Users (_id integer primary key autoincrement, " +
                 "firstName text, lastName text, username text, password text, " +
@@ -94,21 +111,43 @@ public class Model {
         mDb.execSQL(command3);
     }
 
+    /**
+     * clear Users table from SQL database
+     */
     public void clearUsers() {
         String command = "DELETE FROM Users";
         mDb.execSQL(command);
     }
 
+    /**
+     * clear SourceReports table from SQL database
+     */
     public void clearSourceReports() {
         String command = "DELETE FROM SourceReports";
         mDb.execSQL(command);
     }
 
+    /**
+     * clear PurityReports table from SQL database
+     */
     public void clearPurityReports() {
         String command = "DELETE FROM PurityReports";
         mDb.execSQL(command);
     }
 
+    /**
+     * Add user to Users table in Database
+     *
+     * @param firstname string of first name
+     * @param lastname string of last name
+     * @param username string of username
+     * @param password string of password
+     * @param email string of email
+     * @param phone string of phone
+     * @param birthday string of birthday
+     * @param address string of address
+     * @param userType string of user Type
+     */
     public void addUser(String firstname, String lastname, String username, String password,
                         String email, String phone, String birthday, String address,
                         String userType) {
@@ -119,6 +158,14 @@ public class Model {
         mDb.execSQL(command);
     }
 
+    /**
+     * Add report to SourceReports table in Database
+     *
+     * @param lat string of latitude
+     * @param lon string of longitude
+     * @param waterType string of water Type
+     * @param waterCondition string of water condition
+     */
     public void addSourceReport(String lat, String lon, String waterType, String waterCondition) {
         String command = "INSERT INTO SourceReports (latitude, longitude, waterType, " +
                 "waterCondition) " + "VALUES ('" + lon + "', '" + lat + "', '" + waterType +
@@ -126,6 +173,15 @@ public class Model {
         mDb.execSQL(command);
     }
 
+    /**
+     * Add report to PurityReports table in Database
+     *
+     * @param lat string of latitude
+     * @param lon string of longitude
+     * @param overallCondition string of overall condition
+     * @param vPPM string of virus PPM
+     * @param cPPM string of contaminent PPM
+     */
     public void addPurityReport(String lat, String lon, String overallCondition, String vPPM,
                                 String cPPM) {
         String command = "INSERT INTO PurityReports (latitude, longitude, OverallCondition, " +
@@ -134,6 +190,10 @@ public class Model {
         mDb.execSQL(command);
     }
 
+    /**
+     * creates array list of source reports from database
+     * @return List of Source Reports
+     */
     public List<Report> getSourceReportArray() {
         ArrayList<Report> toReturn = new ArrayList<>();
         String query = "SELECT latitude, longitude, waterType, waterCondition FROM SourceReports";
@@ -158,6 +218,11 @@ public class Model {
         return toReturn;
     }
 
+    /**
+     * creates array list of purity reports from database
+     *
+     * @return List<Report> List of Purity Reports
+     */
     public List<Report> getPurityReportArray() {
         ArrayList<Report> toReturn = new ArrayList<>();
         String query = "SELECT latitude, longitude, overallCondition, vPPM, cPPM FROM PurityReports";
@@ -179,6 +244,11 @@ public class Model {
         return toReturn;
     }
 
+    /**
+     * adds an active report to the model and its attributes
+     *
+     * @param report report to set as active
+     */
     public void setActiveReport(Report report) {
         activeReportNumber = report.getReportNumber();
         activeReporterName = report.getReporterName();
@@ -195,6 +265,11 @@ public class Model {
         }
     }
 
+    /**
+     * adds an active report to the model and its attributes
+     *
+     * @param id id of report to set as active
+     */
     public void setActiveReport(int id) {
         List<Report> reports = getSourceReportArray();
         Report report1 = null;
@@ -222,46 +297,94 @@ public class Model {
         return activeDateTime != null;
     }
 
+    /**
+     * gets report number
+     * @return active report number
+     */
     public int getReportNumber() {
         return activeReportNumber;
     }
 
+    /**
+     * gets Date and Time
+     * @return active Date and Time
+     */
     public String getDateTime() {
         return activeDateTime.toString();
     }
 
+    /**
+     * gets latitude
+     * @return active latitude
+     */
     public double getLatitude() {
         return activeLatitude;
     }
 
+    /**
+     * gets longitude
+     * @return active longitude
+     */
     public double getLongitude() {
         return activeLongitude;
     }
 
+    /**
+     * gets water condition
+     * @return active water condition
+     */
     public String getWaterCondition() {
         return activeWaterCondition.toString();
     }
 
+    /**
+     * gets water type
+     * @return active water type
+     */
     public String getWaterType() {
         return activeWaterType.toString();
     }
 
+    /**
+     * gets overall condition
+     * @return active overall condition
+     */
     public String getOverallCondition() {
         return activeOverallCondition.toString();
     }
 
+    /**
+     * gets virus PPM
+     * @return active virus PPM
+     */
     public double getVirusPPM() {
         return activeVPPM;
     }
 
+    /**
+     * gets contaminant PPM
+     * @return active contaminent PPM
+     */
     public double getContaminantPPM() {
         return activeCPPM;
     }
 
+    /**
+     * gets reporter name
+     * @return active reporter name
+     */
     public String getReporterName() {
         return activeReporterName;
     }
 
+    /**
+     * updates user information
+     * @param birthday
+     * @param email
+     * @param phone
+     * @param address
+     * @param userType
+     */
     public void updateUserInfo(String birthday, String email, String phone, String address,
                                String userType) {
         String command = "UPDATE Users SET birthday = '"+ birthday +"', email = '"+ email +
@@ -270,6 +393,11 @@ public class Model {
         mDb.execSQL(command);
     }
 
+    /**
+     * checks whether a username is in the database
+     * @param username
+     * @return whether username is in database
+     */
     public Boolean checkUsername(String username) {
         String query = "SELECT username FROM Users";
         Cursor c = mDb.rawQuery(query, null);
@@ -291,6 +419,14 @@ public class Model {
         return false;
     }
 
+    /**
+     * logs a user into the application
+     * @param username
+     * @param password
+     * @throws InvalidParameterException
+     * @throws NoSuchElementException
+     * @return whether login was successful
+     */
     public Boolean logIn(String username, String password) {
         String query = "SELECT firstName, lastName, username, password, email, phone, birthday, " +
                 "address, userType FROM Users";
@@ -328,63 +464,127 @@ public class Model {
         throw new NoSuchElementException("User not found.");
     }
 
+    /**
+     * gets first name
+     * @return current first name
+     */
     public String getFirstName() {
         return currFirstName;
     }
 
+    /**
+     * gets last name
+     * @return current last name
+     */
     public String getLastName() {
         return currLastName;
     }
 
+    /**
+     * gets username
+     * @return current username
+     */
     public String getUserName() {
         return currUserName;
     }
 
+    /**
+     * gets email
+     * @return active email
+     */
     public String getEmail() {
         return currEmail;
     }
 
+    /**
+     * gets phone
+     * @return current phone
+     */
     public String getPhone() {
         return currPhone;
     }
 
+    /**
+     * gets birthday
+     * @return current birthday
+     */
     public String getBirthday() {
         return currBirthday;
     }
 
+    /**
+     * gets address
+     * @return current address
+     */
     public String getAddress() {
         return currAddress;
     }
 
+    /**
+     * gets user type
+     * @return current user type
+     */
     public String getUserType() {
         return currUserType;
     }
 
+    /**
+     * checks whether current user can file a purity report
+     * @return whether user can file purity report
+     */
     public boolean canFilePurityReport() {
         return currUserType.equals("WORKER") || currUserType.equals("MANAGER");
     }
 
+    /**
+     * checks whether current user can view a purity report
+     * @return whether user can view purity report
+     */
     public boolean canViewPurityReport() {
         return currUserType.equals("MANAGER");
     }
 
+    /**
+     * closes access to database
+     */
     public void close() {
         mDbHelper.close();
     }
 
+    /**
+     * private inner class to access SQL Database
+     */
     private static class DatabaseHelper extends SQLiteOpenHelper {
 
+        // database context
         Context helperContext;
 
+        /**
+         * constructor
+         *
+         * @param context Context of where of the model is used
+         */
         DatabaseHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
             helperContext = context;
         }
 
+        /**
+         * defines database
+         *
+         * @param db SQL database
+         */
         @Override
         public void onCreate(SQLiteDatabase db) {
         }
 
+        /**
+         * handles update of database
+         *
+         * @param db SQL database
+         * @param oldVersion old version of database
+         * @param newVersion new version of database
+         */
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             Log.w(TAG, "Upgrading database!!!!!");
@@ -392,6 +592,11 @@ public class Model {
             onCreate(db);
         }
 
+        /**
+         * creates SQL database
+         *
+         * @throws IOException
+         */
         public void createDataBase() throws IOException {
             boolean dbExist = checkDataBase();
             if (dbExist) {
@@ -400,6 +605,11 @@ public class Model {
             }
         }
 
+        /**
+         * checks whether database can be accessed
+         *
+         * @returns whether database can be accessed
+         */
         private boolean checkDataBase() {
             SQLiteDatabase checkDB = null;
             try {
@@ -414,6 +624,11 @@ public class Model {
             return checkDB != null ? true : false;
         }
 
+        /**
+         * connect to SQL database
+         *
+         * @throws SQLException
+         */
         public void openDataBase() throws SQLException {
             // Open the database
             String myPath = DB_PATH + DATABASE_NAME;
@@ -421,6 +636,9 @@ public class Model {
                     SQLiteDatabase.OPEN_READWRITE);
         }
 
+        /**
+         * closes connection to database
+         */
         @Override
         public synchronized void close() {
 
